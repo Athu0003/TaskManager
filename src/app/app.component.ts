b505ec113ab9a429fc1d4dad54adb0b3d0e91653
Athu0003 <daphalatharva5@gmail.com>
2025-09-13 13:11:58 +0530

    Add Tailwind CSS configuration and update Angular CLI settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaskForm } from './component/task-form/task-form';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [TaskForm],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
